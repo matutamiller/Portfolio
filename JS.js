54d6ec82e3ade2f98ed3ee1136f1eda1f8de4e74
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-04-08 12:48:13 +0200

    worked on 'my pros' block

--- JS.js
+++ JS.js
@@ -60,29 +60,6 @@ function stickyNav() {                // Adding the sticky class to the navbar r
 
 
 
-/* ---------------------------------------------- /*
-* Rotator  
-/* ---------------------------------------------- */
-$(function() {
-  /*
-  - how to call the plugin:
-  $( selector ).cbpQTRotator( [options] );
-   - options:
-  {
-  // default transition speed (ms)
-  speed : 700,
-  // default transition easing
-  easing : 'ease',
-  // rotator interval (ms)
-  interval : 8000
-  }
-  - destroy:
-  $( selector ).cbpQTRotator( 'destroy' );
-  */
-
-  $( '#cbp-qtrotator' ).cbpQTRotator();
-
-});
 
 
 /* ---------------------------------------------- /*
